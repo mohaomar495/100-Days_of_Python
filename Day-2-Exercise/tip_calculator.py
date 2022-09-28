f5b663513f83bdc0862a1ee6071ba4b289558c77
Mohammad Omar <mohaomar495@gmail.com>
2022-09-28 13:59:09 +0300

    calculates the tip

--- Day-2-Exercise/tip_calculator.py
+++ Day-2-Exercise/tip_calculator.py
@@ -6,4 +6,4 @@ percentage_tip = float(input("what percentage would like to give? 10, 12, or 15?
 numb_of_people = int(input("How many people to split the bill? "))
 total = (total_bill / numb_of_people) * ((percentage_tip + 100) / 100)
 
-print(f"each person should pay {total}")
+print(f"each person should pay {round(total, 2)}")

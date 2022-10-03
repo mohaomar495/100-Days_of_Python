d1a01fa18fe6a99b06c8100d71af3c9e5ba0ac26
Mohammad Omar <mohaomar495@gmail.com>
2022-10-03 21:03:35 +0300

    Update day-4-3-exercise.py
    I changed the blank spaces into emoji kind of funny🤣.

--- Day-4-Exercise/day-4-3-exercise.py
+++ Day-4-Exercise/day-4-3-exercise.py
@@ -1,7 +1,7 @@
 # 🚨 Don't change the code below 👇
-row1 = ["⬜️","⬜️","⬜️"]
-row2 = ["⬜️","⬜️","⬜️"]
-row3 = ["⬜️","⬜️","⬜️"]
+row1 = ["🤣","🤣", "🤣"]
+row2 = ["🤣","🤣","🤣"]
+row3 = ["🤣","🤣","🤣"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
